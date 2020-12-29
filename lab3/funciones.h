bf009ecb217f08ba50d76eed1ca862f381dfae51
César <cesar.salazar@usach.cl>
2020-12-28 22:43:56 -0300

    feat: ruido total y potencia

--- lab3/funciones.h
+++ lab3/funciones.h
@@ -1,3 +1,4 @@
+#include <math.h>
 
 void escrituraResultados(char * archivoFinal, int idDisco, int mediaReal, int mediaImag, int potencia, int ruidoTotal){
     FILE* fileFinal = fopen(archivoFinal, "a");
@@ -6,4 +7,25 @@ void escrituraResultados(char * archivoFinal, int idDisco, int mediaReal, int me
     fprintf(fileFinal,"Media imaginaria: %d",mediaImag);
     fprintf(fileFinal,"Potencia: %d",potencia);
     fprintf(fileFinal,"Ruido total %d:",ruidoTotal);
+}
+0 u
+1 v
+2 r
+3 i
+4 w
+int calculoPotenciaParcial(int ** matriz, cantidad){
+    int potenciaParcial = 0;
+    for (int i = 0; i < cantidad; ++i){
+        potenciaParcial += pow(matriz[i][2],2) + sqrt(pow(matriz[i][3],2));
+    }
+    return potenciaParcial;
+}
+
+int calculoRuidoTotalParcial(int ** matriz, cantidad){
+    int ruidoTotalParcial = 0;
+    for (int i = 0; i < cantidad; ++i){
+        potenciaParcial += matriz[i][4];
+    }
+    return potenciaParcial;
+    
 }
