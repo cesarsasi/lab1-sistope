00e0e16eed6dc878016dfa81e9c10730b43f98e5
César <cesar.salazar@usach.cl>
2021-01-03 16:35:43 -0300

    fix: ahora compila almenos

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -10,94 +10,100 @@
 #include "Declaracion.h"
 
 // ./lab3 -i prueba1.csv -o propiedades.txt -d 100 -n 4 -s 10 -b
+
 int main(int argc, char** argv){
 	//Lectura parametros desde la terminal
+    flag=0;
     terminoLectura=0;
-	while (( (d = getopt(argc, argv, "i:o:n:s:b")) != -1)){
-        switch (d)
-        {
-        case 'i':
-        mflag = optarg; 
-        if(optarg == 0){
-            printf("\nIngreso de parametro i incorrecto! \n");
-            exit(EXIT_FAILURE);
-        }else{
-            archivoVisibilidades = optarg;
-            FILE* fileVisib = fopen(archivoVisibilidades, "r");
-            if(fileVisib==NULL){
-            printf("\nIngreso de parametro i (nombre archivo de vidibilidades) incorrecto!, el archivo no existe \n");
-            exit(EXIT_FAILURE);
-            }
-            fclose(fileVisib);
+	while (( (d = getopt(argc, argv, "i:o:d:n:s:b")) != -1)){
+        switch (d){
+            case 'i':
+                mflag = optarg; 
+                if(optarg == 0){
+                    printf("\nIngreso de parametro i incorrecto! \n");
+                    exit(EXIT_FAILURE);
+                }else{
+                    archivoVisibilidades = optarg;
+                    FILE* fileVisib = fopen(archivoVisibilidades, "r");
+                    if(fileVisib==NULL){
+                        printf("\nIngreso de parametro i (nombre archivo de vidibilidades) incorrecto!, el archivo no existe \n");
+                        exit(EXIT_FAILURE);
+                    }
+                    fclose(fileVisib);
+                }
+                break;
+            case 'o':
+                mflag = optarg; 
+                if(optarg == 0){
+                    printf("\nIngreso de parametro o incorrecto! \n");
+                    exit(EXIT_FAILURE);
+                }else{
+                    archivoSalida = optarg;
+                    FILE* fileSalida = fopen(archivoSalida, "w");
+                    if(fileSalida==NULL){
+                        printf("\nIngreso de parametro o (nombre archivo de salida) incorrecto!, el archivo no existe \n");
+                        exit(EXIT_FAILURE);
+                    }
+                    fclose(fileSalida);
+                }
+                break;
+            case 'd':
+                mflag = optarg; 
+                if(optarg == 0){
+                    printf("\nIngreso de parametro d (ancho de discos) incorrecto!\n");
+                    exit(EXIT_FAILURE);
+                }else{
+                    anchoDiscos = atoi(optarg);
+                    if(anchoDiscos >= 0){
+                        break;
+                    }else{
+                        printf("\nIngreso de parametro d (ancho de discos) incorrecto!, debe ser mayor que cero\n"); //Verificacion pendiente
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                break;
+            case 'n':
+                mflag = optarg; 
+                if(optarg == 0){
+                    printf("\nIngreso de parametro n (cantidad de discos) incorrecto!\n");
+                    exit(EXIT_FAILURE);
+                }else{
+                    cantDiscos = atoi(optarg);
+                    if(cantDiscos >= 0){
+                        break;
+                    }else{
+                        printf("\nIngreso de parametro n (cantidad de discos) incorrecto!, debe ser mayor que cero\n"); //Verificacion pendiente
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                break;   
+            case 's':
+                mflag = optarg; 
+                if(optarg == 0){
+                    printf("\nIngreso de parametro s (buffer) incorrecto!\n");
+                    exit(EXIT_FAILURE);
+                }else{
+                    buffer = atoi(optarg);
+                    if(buffer >= 0){
+                        break;
+                    }else{
+                        printf("\nIngreso de parametro s (buffer) incorrecto!, debe ser igual o mayor que cero\n"); //Verificacion pendiente
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                break;
+            case 'b':
+                flag = 1;
+                break;
         }
-        break;
-        case 'o':
-        mflag = optarg; 
-        if(optarg == 0){
-            printf("\nIngreso de parametro o incorrecto! \n");
-            exit(EXIT_FAILURE);
-        }else{
-            archivoSalida = optarg;
-            FILE* fileSalida = fopen(archivoSalida, "w");
-            if(fileSalida==NULL){
-            printf("\nIngreso de parametro o (nombre archivo de salida) incorrecto!, el archivo no existe \n");
-            exit(EXIT_FAILURE);
-            }
-            fclose(fileSalida);
-        }
-        break;
-        case 'n':
-        mflag = optarg; 
-        if(optarg == 0){
-            printf("\nIngreso de parametro n (cantidad de discos) incorrecto!\n");
-            exit(EXIT_FAILURE);
-        }else{
-            cantDiscos = atoi(optarg);
-            if(cantDiscos >= 0){
-            break;
-            }
-            else{
-            printf("\nIngreso de parametro n (cantidad de discos) incorrecto!, debe ser mayor que cero\n"); //Verificacion pendiente
-            exit(EXIT_FAILURE);
-            }
-        }
-        case 'd':
-        mflag = optarg; 
-        if(optarg == 0){
-            printf("\nIngreso de parametro d (ancho de discos) incorrecto!\n");
-            exit(EXIT_FAILURE);
-        }else{
-            anchoDiscos = atoi(optarg);
-            if(anchoDiscos >= 0){
-            break;
-            }
-            else{
-            printf("\nIngreso de parametro d (ancho de discos) incorrecto!, debe ser mayor que cero\n"); //Verificacion pendiente
-            exit(EXIT_FAILURE);
-            }
-        }
-        break;      
-        case 's':
-        mflag = optarg; 
-        if(optarg == 0){
-            printf("\nIngreso de parametro s (buffer) incorrecto!\n");
-            exit(EXIT_FAILURE);
-        }else{
-            buffer = atoi(optarg);
-            if(buffer >= 0){
-            break;
-            }
-            else{
-            printf("\nIngreso de parametro s (buffer) incorrecto!, debe ser igual o mayor que cero\n"); //Verificacion pendiente
-            exit(EXIT_FAILURE);
-            }
-        }
-        break;
-        case 'b':
-        flag = 1;
-        break;
-        }
+        
     }
+    printf("\n i - %s",archivoVisibilidades);
+    printf("\n o - %s",archivoSalida);
+    printf("\n d - %d",anchoDiscos);
+    printf("\n n - %d",cantDiscos);
+    printf("\n s - %d",buffer);
+    printf("\n b - %d",flag);
 
     //Inicializar estructura para resultados totales (comun)
     iniciarEstructuraComun();
